e40139ad8f122aa4ccb0c46209a8039dbb6e468d
gaopuguang <2694484453@qq.com>
2025-11-20 00:06:17 +0800

    优化

--- src/pages/git/list/index.vue
+++ src/pages/git/list/index.vue
@@ -12,8 +12,8 @@
       >
         <t-row justify="space-between">
           <div class="left-operation-container">
-            <t-button @click="handleSetupContract">导入</t-button>
-            <t-button variant="base" theme="default" :disabled="!selectedRowKeys.length"> 导出</t-button>
+            <t-button @click="handleSetupContract">添加</t-button>
+            <!--            <t-button variant="base" theme="default" :disabled="!selectedRowKeys.length"> 导出</t-button>-->
             <!--            <p v-if="!!selectedRowKeys.length" class="selected-count">已选{{ selectedRowKeys.length }}项</p>-->
           </div>
           <t-input v-model="searchValue" class="search-input" placeholder="请输入你需要搜索的内容" clearable>
@@ -22,7 +22,7 @@
             </template>
           </t-input>
           <t-col :span="2" class="operation-container">
-            <t-button theme="primary" :style="{ marginLeft: '8px' }" @click="getList"> 查询</t-button>
+            <t-button theme="primary" :style="{ marginLeft: '8px' }"> 查询</t-button>
             <t-button type="reset" variant="base" theme="default"> 重置</t-button>
           </t-col>
         </t-row>
@@ -54,7 +54,7 @@
             <a :href="row.html_url" target="_blank">{{ row.html_url }}</a>
           </template>
           <template #op="slotProps">
-            <a class="t-button-link" @click="handleClickSuccess()">在线预览</a>
+            <!--            <a class="t-button-link" @click="handleClickSuccess()">在线开发</a>-->
             <a class="t-button-link" @click="handleClickDetail(slotProps)">详情</a>
             <a class="t-button-link" @click="handleClickDelete(slotProps.row)">删除</a>
           </template>
@@ -63,9 +63,9 @@
     </t-card>
     <div>
       <t-pagination
-        v-model="formData.pageNum"
+        v-model="searchForm.pageNum"
         :total="pagination.total"
-        :page-size.sync="formData.pageSize"
+        :page-size.sync="searchForm.pageSize"
         @current-change="onCurrentChange"
         @page-size-change="onPageSizeChange"
         @change="onChange"/>
@@ -78,72 +78,52 @@
       :onCancel="onCancel"
     >
     </t-dialog>
-    <!--git-->
-    <t-dialog
-      :visible.sync="dialog.visible"
-      :header="dialog.header"
-      width="60%"
-      :confirm-on-enter="true"
-      :on-cancel="onCancel"
-      :on-close-btn-click="onCloseDialogBtnClick"
-      :on-confirm="onConfirmDialog"
+    <!--抽屉-->
+    <t-drawer
+      :visible.sync="drawer.visible"
+      :header="drawer.header"
+      :on-overlay-click="() => (drawer.visible = false)"
+      :on-size-drag-end="handleSizeDrag"
+      showOverlay
+      :sizeDraggable="true"
+      placement="right"
+      destroyOnClose
+      size="30%"
+      @close="onCancelDrawer"
+      :onConfirm="handleDrawerOk"
+      @cancel="onCancelDrawer"
     >
-      <t-card class="list-card-container" :bordered="false">
-        <div>
-            <t-form
-              ref="form"
-              :data="formData"
-              label-align="left"
-              @reset="onReset"
-              @submit="onSubmit"
-              layout="inline"
-            >
-              <div class="left-operation-container">
-                <t-form-item label="名称" name="name">
-                  <t-input v-model="dialog.name" style="width: 250px" class="search-input" placeholder="请输入你需要搜索的内容" clearable>
-                    <template #suffix-icon>
-                      <search-icon size="18px"/>
-                    </template>
-                  </t-input>
-                </t-form-item>
-                <t-form-item label="类型" name="type">
-                  <t-select
-                    v-model="dialog.type"
-                    :style="{ width: '120px' }"
-                    placeholder="请选择类型"
-                    class="demo-select-base"
-                    clearable>
-                    <t-option v-for="(item, index) in dialog.gitRepoTypeList" :key="index" :value="item" :label="item">
-                      {{ item }}
-                    </t-option>
-                  </t-select>
-                </t-form-item>
-              </div>
-              <div class="right-operation-container">
-                <t-button theme="primary" :style="{ marginLeft: '8px' }" @click="getRepoList"> 查询</t-button>
-                <t-button type="reset" variant="base" theme="default"> 重置</t-button>
-              </div>
-            </t-form>
-          <t-table
-            :columns="dialog.gitRepoColumns"
-            :data="dialog.gitRepoList"
-            :rowKey="rowKey"
-            :selected-row-keys="selectedRowKeys"
-            :loading="dataLoading"
-            :headerAffixedTop="true"
-            :headerAffixProps="{ offsetTop: offsetTop, container: getContainer }"
-          >
-            <template #html_url="{row}">
-              <a :href="row.html_url" target="_blank">{{row.html_url}}</a>
-            </template>
-            <template #op="slotProps">
-              <a class="t-button-link" @click="importRepo(slotProps.row)">导入</a>
-            </template>
-          </t-table>
-        </div>
-        <t-pagination v-model="dialog.current" v-model:pageSize="dialog.pageSize" :total="dialog.total"/>
-      </t-card>
-    </t-dialog>
+      <t-space v-show="drawer.operation === 'add'|| drawer.operation ==='edit'" direction="vertical" style="width: 100%">
+        <t-form
+          ref="formValidatorStatus"
+          :data="formData"
+          :label-width="100"
+          @reset="onReset"
+        >
+          <t-form-item label="Git地址" name="url">
+            <t-input v-model="formData.url" style="width: 250px" class="search-input" placeholder="" clearable></t-input>
+          </t-form-item>
+          <t-form-item label="仓库名称" name="name">
+            <t-input v-model="formData.name" style="width: 250px" class="search-input" placeholder="" clearable></t-input>
+          </t-form-item>
+          <t-form-item label="类型" name="type">
+            <t-select
+              v-model="formData.type"
+              :style="{ width: '120px' }"
+              placeholder="请选择类型"
+              class="demo-select-base"
+              clearable>
+              <t-option v-for="(item, index) in gitRepoTypeList" :key="index" :value="item" :label="item">
+                {{ item }}
+              </t-option>
+            </t-select>
+          </t-form-item>
+          <t-form-item label="描述" name="name">
+            <t-textarea v-model="formData.description" style="width: 250px" class="search-input" placeholder="" clearable maxlength="256"></t-textarea>
+          </t-form-item>
+        </t-form>
+      </t-space>
+    </t-drawer>
   </div>
 </template>
 <script lang="ts">
@@ -226,7 +206,7 @@ export default Vue.extend({
         {
           align: 'left',
           fixed: 'right',
-          width: 200,
+          width: 120,
           colKey: 'op',
           title: '操作',
         },
@@ -245,57 +225,36 @@ export default Vue.extend({
       searchValue: '',
       confirmVisible: false,
       deleteIdx: -1,
-      formData: {
+      // 搜索框
+      searchForm: {
         name: "",
         type: "",
         pageNum: 1,
-        pageSize: 10,
-        params: {}
+        pageSize: 10
       },
-      dialog: {
-        visible: false,
-        header: "仓库导入",
-        current: 1,
-        pageSize: 10,
-        total: 0,
-        type: "gitee",
+      formData: {
         name: "",
-        gitRepoList: [],
-        gitRepoTypeList: [],
-        gitRepoColumns: [
-          {
-            title: '仓库名称',
-            align: 'left',
-            width: 150,
-            ellipsis: true,
-            colKey: 'name',
-          },
-          {
-            title: '语言',
-            colKey: 'language',
-            width: 120
-          },
-          {
-            title: '状态',
-            width: 60,
-            ellipsis: true,
-            colKey: 'status',
-          },
-          {
-            title: '地址',
-            width: 160,
-            ellipsis: true,
-            colKey: 'html_url',
-          },
-          {
-            align: 'left',
-            fixed: 'right',
-            width: 60,
-            colKey: 'op',
-            title: '操作',
-          },
-        ]
-      }
+        url: "",
+        type: "",
+        description: "",
+      },
+      // 对话框
+      confirm: {
+        header: "",
+        body: "",
+        operation: "update",
+        visible: false
+      },
+      // 抽屉
+      drawer: {
+        header: "",
+        visible: false,
+        type: "",
+        operation: "add",
+        row: {},
+        dynamicForm: {}
+      },
+      gitRepoTypeList: []
     };
   },
   computed: {
@@ -313,29 +272,26 @@ export default Vue.extend({
   mounted() {
   },
   created() {
-    this.getList()
+    this.page()
+  },
+  watch:{
+    "searchForm.name"(newVal, oldVal) {
+      if (newVal != oldVal) {
+        this.page()
+      }
+    },
+    "searchForm.pageSize"(newVal, oldVal) {
+      if (newVal != oldVal) {
+        this.page()
+      }
+    },
+    "searchForm.pageNum"(newVal, oldVal) {
+      if (newVal != oldVal) {
+        this.page()
+      }
+    }
   },
   methods: {
-    getList() {
-      this.dataLoading = true;
-      this.$request.get('/git/repo/page', {
-        params: this.formData
-      }).then((res) => {
-        if (res.data.code === 200) {
-          this.data = res.data.rows;
-          this.pagination = {
-            ...this.pagination,
-            total: res.data.total,
-          };
-        }
-      })
-        .catch((e: Error) => {
-          console.log(e);
-        })
-        .finally(() => {
-          this.dataLoading = false;
-        });
-    },
     getRepoList() {
       this.dataLoading = true;
       this.$request.get('/git/accessRepo/page', {
@@ -346,7 +302,7 @@ export default Vue.extend({
       }).then((res) => {
         if (res.data.code === 200) {
           console.log("repos", res.data.rows)
-          this.dialog.gitRepoList = res.data.rows;
+          this.gitRepoList = res.data.rows;
           this.dialog.total = res.data.total;
         }
       }).catch((e: Error) => {
@@ -360,7 +316,7 @@ export default Vue.extend({
       this.$request.get('/git/common/types').then((res) => {
         if (res.data.code === 200) {
           console.log("types", res.data.data)
-          this.dialog.gitRepoTypeList = res.data.data;
+          this.gitRepoTypeList = res.data.data;
         }
       })
     },
@@ -406,13 +362,14 @@ export default Vue.extend({
       console.log('Page Info: ', pageInfo);
     },
     handleClickDetail(row) {
-      //this.$router.push('/build/helmDetail');
-      this.$emit('transfer', "detail", row)
+      this.formData = row;
+      this.drawer.operation = 'detail';
+      this.drawer.visible = true;
     },
     // 点击导入
     handleSetupContract() {
-      this.dialog.visible = true
-      this.getRepoList()
+      this.drawer.operation = 'add'
+      this.drawer.visible = true
       this.getRepoTypeList()
     },
     // 点击删除
@@ -431,7 +388,7 @@ export default Vue.extend({
     // 确认删除
     onConfirmDelete() {
       // 真实业务请发起请求
-      this.$request.delete("/git/repo/delete?id="+ this.formData.params.id).then(res=>{
+      this.$request.delete("/git/repo/delete?id=" + this.formData.params.id).then(res => {
         if (res.data.code === 200) {
           this.$message.success(res.data.msg)
           this.confirmVisible = false;
@@ -448,18 +405,70 @@ export default Vue.extend({
     onCloseDialogBtnClick() {
 
     },
+    // 确认抽屉
+    handleDrawerOk() {
+        switch (this.drawer.operation) {
+          case 'add':
+            this.$request.post('/git/repo/add',this.formData).then((res) => {
+              if (res.data.code === 200) {
+                console.log(res.data.data);
+                this.$message.success(res.data.msg);
+              } else  {
+                this.$message.error(res.data.msg);
+              }
+            }).catch((err) => {
+
+            }).finally(() => {
+
+            })
+            break;
+          case 'edit':
+            break;
+          case 'detail':
+            break;
+        }
+    },
+    // 取消删除
     onCancel() {
-      this.resetIdx();
+      this.confirm.visible = false;
+      this.dataLoading = false;
+      this.$message.info("取消删除！");
+    },
+    // 取消抽屉
+    onCancelDrawer() {
+      this.drawer.visible = false;
+      this.dataLoading = false;
+    },
+    // drawer大小
+    handleSizeDrag({size}) {
+      console.log('size drag size: ', size);
     },
     resetIdx() {
       this.deleteIdx = -1;
     },
     onReset(data) {
-      console.log(data);
+      this.searchForm = {}
     },
     onSubmit(data) {
-      console.log(this.formData);
-      this.getList(this.formData);
+      this.page();
+    },
+    page() {
+      this.dataLoading = true;
+      this.$request.get('/git/repo/page', {
+        params: this.searchForm
+      }).then((res) => {
+        if (res.data.code === 200) {
+          this.data = res.data.rows;
+          this.pagination = {
+            ...this.pagination,
+            total: res.data.total,
+          };
+        }
+      }).catch((e: Error) => {
+        console.log(e);
+      }).finally(() => {
+        this.dataLoading = false;
+      });
     },
   },
 });
