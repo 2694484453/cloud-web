e4b3ce6a15e16f47e9f7f1eb878944df36de62c6
gaopuguang <2694484453@qq.com>
2025-11-25 18:17:20 +0800

    11/21/23

--- src/pages/traefik/index.vue
+++ src/pages/traefik/index.vue
@@ -1,39 +1,144 @@
 <template>
-  <div>
-    <ListBase v-show="type === 'list'" @transfer="changeKey"/>
-    <FormBase v-show="type === 'form'" @transfer="changeKey"/>
-    <EditBase v-show="type === 'edit'" :row="row"  @transfer="changeKey"/>
-    <DetailBase v-show="type === 'detail'" :row="row" @transfer="changeKey"/>
+  <div class="dashboard-detail">
+    <TopCard :data="overViewData"/>
+    <div style="margin-top: 15px">
+      <NoticeCard :data="sysNoticeData"/>
+    </div>
   </div>
 </template>
-<script>
-import ListBase from "@/pages/traefik/list/index.vue"
-import FormBase from "@/pages/traefik/form/base/index.vue"
-import EditBase from "@/pages/traefik/edit/base/index.vue"
-import DetailBase from "@/pages/traefik/detail/base/index.vue";
-
+<script lang="ts">
+import TopCard from "@/components/top-card/TopCard.vue";
+import NoticeCard from "@/components/notice-card/NoticeCard.vue";
 export default {
-  components: {
-    ListBase,
-    FormBase,
-    EditBase,
-    DetailBase
-  },
+  name: 'DashboardDetail',
+  components: {NoticeCard, TopCard},
   data() {
     return {
-      type: "list",
-      row: Object
+      overViewData: [],
+      sysNoticeData: [],
     };
   },
+  computed: {
+  },
+  watch: {
+
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.updateContainer();
+    });
+    this.renderCharts();
+  },
+  created() {
+    this.overView();
+    this.getNotice();
+  },
   methods: {
-    changeKey(key, row) {
-      console.log("key:id", key, row)
-      this.type = key
-      this.row = row.row
+    overView() {
+      this.$request.get("/traefik/overView", {}).then((res) => {
+        this.overViewData = res.data.data;
+      }).catch((err) => {
+        console.log(err);
+      }).finally(() => {
+
+      })
+    },
+    getNotice() {
+      this.$request.get('/sysNotice/page', {
+        params: {
+          type: 'traefik',
+        }
+      }).then((res) => {
+        if (res.data.code === 200) {
+          this.sysNoticeData = res.data.rows;
+        }
+      }).catch((e: Error) => {
+        console.log(e);
+      }).finally(() => {
+        this.dataLoading = false;
+      });
+    }
+  },
+};
+</script>
+<style lang="less" scoped>
+@import '@/style/variables.less';
+
+.row-margin {
+  margin-top: 16px;
+}
+
+// 统一增加8px;
+.dashboard-detail-card {
+  padding: 8px;
+
+  /deep/ .t-card__title {
+    font-size: 20px;
+    font-weight: 500;
+  }
+
+  /deep/ .t-card__actions {
+    display: flex;
+    align-items: center;
+  }
+}
+
+.dashboard-list-card {
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  height: 170px;
+  padding: 8px;
+
+  /deep/ .t-card__header {
+    padding-bottom: 8px;
+  }
+
+  /deep/ .t-card__body {
+    flex: 1;
+    display: flex;
+    padding-top: 0;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+
+  &.dark {
+    &:hover {
+      background: var(--td-gray-color-14);
+      cursor: pointer;
+    }
+  }
+
+  &.light {
+    &:hover {
+      background: var(--td-gray-color-14);
+      cursor: pointer;
+    }
+  }
+
+  &__number {
+    font-size: 36px;
+    line-height: 44px;
+    color: var(--td-text-color-primary);
+  }
+
+  &__text {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 14px;
+    color: var(--td-text-color-placeholder);
+    text-align: left;
+    line-height: 18px;
+
+    &-left {
+      display: flex;
+
+      .icon {
+        margin: 0 8px;
+      }
     }
   }
 }
-</script>
-<style scoped lang="less">
-
 </style>
