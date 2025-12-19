f6b95771d5c4bc756624ae3f5f0351148df48503
gaopuguang <2694484453@qq.com>
2025-12-20 01:14:29 +0800

    调整

--- src/pages/app/manager/list/index.vue
+++ src/pages/app/manager/list/index.vue
@@ -294,7 +294,7 @@ export default Vue.extend({
     },
     getList() {
       this.dataLoading = true;
-      this.$request.get('/helm/page',{
+      this.$request.get('/app/manager/page',{
           params: this.formData
         }).then((res) => {
         if (res.data.code === 200) {
