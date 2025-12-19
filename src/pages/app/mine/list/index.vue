22b56091ee49b764e1f442705db1292532be0e69
gaopuguang <2694484453@qq.com>
2025-12-19 23:07:16 +0800

    调整

--- src/pages/app/mine/list/index.vue
+++ src/pages/app/mine/list/index.vue
@@ -12,9 +12,9 @@
       >
         <t-row justify="space-between">
           <div class="left-operation-container">
-            <t-button @click="handleSetupContract">安装应用</t-button>
-<!--            <t-button variant="base" theme="default" :disabled="!selectedRowKeys.length">导出应用</t-button>-->
-<!--            <p v-if="!!selectedRowKeys.length" class="selected-count">已选{{ selectedRowKeys.length }}项</p>-->
+            <t-button @click="handleSetupContract">自定义安装</t-button>
+            <!--            <t-button variant="base" theme="default" :disabled="!selectedRowKeys.length">导出应用</t-button>-->
+            <!--            <p v-if="!!selectedRowKeys.length" class="selected-count">已选{{ selectedRowKeys.length }}项</p>-->
           </div>
           <t-input v-model="searchForm.name" class="search-input" placeholder="请输入你需要搜索的内容" clearable>
             <template #suffix-icon>
@@ -40,19 +40,18 @@
           :headerAffixedTop="true"
           :headerAffixProps="{ offsetTop: offsetTop, container: getContainer }"
         >
-          <template #status="{ row }">
-            <t-tag v-if="row.status === CONTRACT_STATUS.FAIL" theme="danger" variant="light">审核失败</t-tag>
-            <t-tag v-if="row.status === CONTRACT_STATUS.AUDIT_PENDING" theme="warning" variant="light">待审核</t-tag>
-            <t-tag v-if="row.status === CONTRACT_STATUS.EXEC_PENDING" theme="warning" variant="light">待履行</t-tag>
-            <t-tag v-if="row.status === CONTRACT_STATUS.EXECUTING" theme="success" variant="light">履行中</t-tag>
-            <t-tag v-if="row.status === CONTRACT_STATUS.FINISH" theme="success" variant="light">已完成</t-tag>
+          <template #status="{row}">
+            <t-tag v-if="row.status === 'fail'" theme="warning" variant="light">安装失败</t-tag>
+            <t-tag v-if="row.status === 'error'" theme="danger" variant="light">错误</t-tag>
+            <t-tag v-if="row.status === null || row.status ===''" theme="default" variant="light">未知</t-tag>
+            <t-tag v-if="row.status === 'ok'" theme="success" variant="light">正常</t-tag>
           </template>
-          <template #contractType="{ row }">
+          <template #contractType="{row}">
             <p v-if="row.contractType === CONTRACT_TYPES.MAIN">审核失败</p>
             <p v-if="row.contractType === CONTRACT_TYPES.SUB">待审核</p>
             <p v-if="row.contractType === CONTRACT_TYPES.SUPPLEMENT">待履行</p>
           </template>
-          <template #paymentType="{ row }">
+          <template #paymentType="{row}">
             <p v-if="row.paymentType === CONTRACT_PAYMENT_TYPES.PAYMENT" class="payment-col">
               付款
               <trend class="dashboard-item-trend" type="up"/>
@@ -65,7 +64,7 @@
           <template #op="slotProps">
             <a class="t-button-link" @click="handleClickDetail(slotProps.row)">详情</a>
             <a class="t-button-link" @click="handleClickEdit(slotProps.row)">管理</a>
-            <a class="t-button-link" @click="handleClickConfirm(slotProps.row)">卸载</a>
+            <a class="t-button-link" @click="handleClickDelete(slotProps.row)">卸载</a>
           </template>
         </t-table>
       </div>
@@ -83,7 +82,7 @@
       :header="confirm.header"
       :body="confirm.body"
       :visible.sync="confirm.visible"
-      @confirm="handleSubmit"
+      @confirm="handleConfirmOk"
       :onCancel="onCancel">
     </t-dialog>
     <t-drawer
@@ -99,56 +98,63 @@
       @close="onCancelDrawer"
       :onConfirm="handleDrawerOk"
       @cancel="onCancelDrawer">
-      <t-space v-show="drawer.operation === 'add'|| drawer.operation ==='edit'"  direction="vertical" style="width: 100%">
+      <t-space v-show="drawer.operation === 'add'|| drawer.operation ==='edit'" direction="vertical"
+               style="width: 100%">
         <t-form
           ref="formValidatorStatus"
           :data="formData"
           :label-width="120"
           @reset="onReset"
         >
-          <t-form-item label="应用名称" name="branch">
-            <t-input v-model="formData.appName" placeholder="请输入英文字母和数字的组合名称" :maxlength="64" with="200" clearable></t-input>
+          <t-form-item label="应用发布名称" name="branch">
+            <t-input v-model="formData.releaseName" placeholder="请输入英文字母和数字的组合名称" :maxlength="64"
+                     with="200" clearable></t-input>
           </t-form-item>
           <t-form-item label="命名空间" name="localIp">
             <t-input v-model="formData.nameSpace" :maxlength="64" with="200" clearable></t-input>
           </t-form-item>
           <t-form-item label="集群" name="kubeContext">
             <t-select v-model="formData.kubeContext" placeholder="请选择" clearable>
-              <t-option v-for="(item,index) in clusters" :key="index" :label="item.contextName" :value="item.contextName" >{{item.contextName}}</t-option>
+              <t-option v-for="(item,index) in clusters" :key="index" :label="item.contextName"
+                        :value="item.contextName">{{ item.contextName }}
+              </t-option>
             </t-select>
           </t-form-item>
           <t-form-item label="安装包地址" name="chartUrl">
-            <t-select
-              v-model="formData.chartUrl"
-              placeholder="支持输入和选择"
-              clearable
-              :options="packages"
-              @input-change="packageList"
-              allow-input
-              allow-create
-              default-first-option
-              filterable
-              @blur="onBlur"
-              :keys="{ label: 'name', value: 'url' }" >
-            </t-select>
+            <t-input v-model="formData.chartUrl" placeholder="请输入安装包的url地址" clearable></t-input>
+            <!--            <t-select-->
+            <!--              v-model="formData.chartUrl"-->
+            <!--              placeholder="支持输入和选择"-->
+            <!--              clearable-->
+            <!--              :options="packages"-->
+            <!--              @input-change="packageList"-->
+            <!--              allow-input-->
+            <!--              allow-create-->
+            <!--              default-first-option-->
+            <!--              filterable-->
+            <!--              @blur="onBlur"-->
+            <!--              :keys="{ label: 'name', value: 'url' }" >-->
+            <!--            </t-select>-->
           </t-form-item>
           <t-form-item label="参数" name="description">
-            <t-textarea v-model="formData.chartValues" placeholder="请输入备注内容" :maxlength="9999" with="200" :autosize="{minRows:5}"></t-textarea>
+            <t-textarea v-model="formData.chartValues" placeholder="请输入备注内容" :maxlength="9999" with="200"
+                        :autosize="{minRows:7}"></t-textarea>
           </t-form-item>
           <t-form-item label="描述" name="description">
-            <t-textarea v-model="formData.description" placeholder="请输入备注内容" :maxlength="200" with="200" :autosize="{minRows:2}"></t-textarea>
+            <t-textarea v-model="formData.description" placeholder="请输入备注内容" :maxlength="200" with="200"
+                        :autosize="{minRows:2}"></t-textarea>
           </t-form-item>
         </t-form>
       </t-space>
-      <t-space v-show="drawer.operation === 'detail'" direction="vertical" style="width: 100%" >
+      <t-space v-show="drawer.operation === 'detail'" direction="vertical" style="width: 100%">
         <t-descriptions bordered :layout="'vertical'" :item-layout="'horizontal'" :column="3">
-          <t-descriptions-item label="应用名称" >{{formData.appName}}</t-descriptions-item>
-          <t-descriptions-item label="命名空间">{{formData.nameSpace}}</t-descriptions-item>
-          <t-descriptions-item label="版本">{{formData.version}}</t-descriptions-item>
-          <t-descriptions-item label="状态">{{formData.status}}</t-descriptions-item>
-          <t-descriptions-item label="创建时间">{{formData.createTime}}</t-descriptions-item>
-          <t-descriptions-item label="创建者">{{formData.createBy}}</t-descriptions-item>
-          <t-descriptions-item label="描述">{{formData.description}}</t-descriptions-item>
+          <t-descriptions-item label="应用名称">{{ formData.appName }}</t-descriptions-item>
+          <t-descriptions-item label="命名空间">{{ formData.nameSpace }}</t-descriptions-item>
+          <t-descriptions-item label="版本">{{ formData.version }}</t-descriptions-item>
+          <t-descriptions-item label="状态">{{ formData.status }}</t-descriptions-item>
+          <t-descriptions-item label="创建时间">{{ formData.createTime }}</t-descriptions-item>
+          <t-descriptions-item label="创建者">{{ formData.createBy }}</t-descriptions-item>
+          <t-descriptions-item label="描述">{{ formData.description }}</t-descriptions-item>
         </t-descriptions>
       </t-space>
     </t-drawer>
@@ -182,7 +188,7 @@ export default Vue.extend({
       columns: [
         {colKey: 'row-select', type: 'multiple', width: 64, fixed: 'left'},
         {
-          title: '发布名称',
+          title: '应用发布名称',
           align: 'left',
           width: 120,
           ellipsis: true,
@@ -268,6 +274,7 @@ export default Vue.extend({
         home: "",
         icon: "",
         nameSpace: '',
+        releaseName: '',
         version: '',
         type: "",
         description: "",
@@ -293,7 +300,7 @@ export default Vue.extend({
         visible: false
       },
       // 搜索框
-      searchForm:{
+      searchForm: {
         name: "",
         version: "",
         pageNum: 1,
@@ -313,7 +320,7 @@ export default Vue.extend({
   created() {
     this.page();
   },
-  watch:{
+  watch: {
     "searchForm.name"(newVal, oldVal) {
       if (newVal != oldVal) {
         this.page();
@@ -350,7 +357,7 @@ export default Vue.extend({
     },
     // 确认抽屉
     handleDrawerOk() {
-      console.log('执行:',this.operation);
+      console.log('执行:', this.operation);
       switch (this.operation) {
         case 'add':
           break;
@@ -362,7 +369,7 @@ export default Vue.extend({
             if (res.data.code === 200) {
               this.$message.success(res.data.msg);
               this.handleSubmit("search")
-            } else  {
+            } else {
               this.$message.error(res.data.msg);
             }
           }).catch((e: Error) => {
@@ -391,11 +398,11 @@ export default Vue.extend({
       console.log('统一Change', changeParams, triggerAndData);
     },
     // 详情
-    handleClickDetail(row:any) {
+    handleClickDetail(row: any) {
       this.formData = row;
       this.drawer.header = row.appName;
       this.drawer.operation = 'detail';
-      this.drawer.size = '40%'
+      this.drawer.size = '30%'
       this.drawer.visible = true;
     },
     // 创建应用
@@ -409,29 +416,37 @@ export default Vue.extend({
       this.packageList();
     },
     // 编辑
-    handleClickEdit(row:any) {
+    handleClickEdit(row: any) {
       this.formData = row;
       this.drawer.header = row.appName;
       this.drawer.operation = 'edit';
       this.drawer.size = '40%'
       this.drawer.visible = true;
     },
-    handleClickDelete(row: { rowIndex: any }) {
-      this.deleteIdx = row.rowIndex;
-      this.confirmVisible = true;
+    handleClickDelete(row: any) {
+      this.formData = row;
+      this.confirm.operation = 'delete';
+      this.confirm.header = "删除：" + this.formData.appName;
+      this.confirm.body = "确认删除吗？一旦删除数据无法恢复";
+      this.confirm.visible = true;
     },
     // 对话框信息自定义
-    handleClickConfirm(row) {
-      this.formData = row
-      switch(this.operation) {
+    handleConfirmOk() {
+      switch (this.confirm.operation) {
         case 'add':
           break;
         case 'update':
           break;
         case 'delete':
-          this.confirm.visible = true;
-          this.confirm.header = "删除：" + this.formData.appName;
-          this.confirm.body = "确认删除吗？一旦删除数据无法恢复";
+          this.$request.delete('/app/mine/delete?id=' + this.formData.id).then((res) => {
+            if (res.data.code === 200) {
+              this.$message.success(res.data.msg);
+              this.page();
+              this.confirm.visible = false;
+            } else {
+              this.$message.error(res.data.msg);
+            }
+          })
           break;
       }
     },
@@ -480,7 +495,7 @@ export default Vue.extend({
             if (res.data.code === 200) {
               this.$message.success(res.data.msg);
               this.handleSubmit("search")
-            } else  {
+            } else {
               this.$message.error(res.data.msg);
             }
           }).catch((e: Error) => {
@@ -499,7 +514,7 @@ export default Vue.extend({
         if (res.data.code === 200) {
           this.data = res.data.rows;
           this.pagination.total = res.data.total;
-        }else {
+        } else {
           this.$message.error(res.data.msg);
         }
       }).catch((e: Error) => {
@@ -527,7 +542,7 @@ export default Vue.extend({
       }).then((res) => {
         if (res.data.code === 200) {
           this.packages = res.data.data;
-        } else  {
+        } else {
           this.$message.error(res.data.msg);
         }
       })
